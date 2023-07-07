fb97623800fe5fd5ade679586f39f48aaa954f7d
alannguyen2003 <nguyenho30112003@gmail.com>
2023-07-07 12:04:39 +0700

    testing ci

--- src/test/java/org/alannguyen/steps/MyStepdefs.java
+++ src/test/java/org/alannguyen/steps/MyStepdefs.java
@@ -8,26 +8,32 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static junit.framework.TestCase.assertEquals;
+
 
 public class MyStepdefs {
 
-    private WebDriver webDriver;
+    private WebDriver webDriver = new ChromeDriver();
 
     @Given("^User enters \"(.*)\" and \"(.*)\"$")
     public void userEntersUsernameAndPassword(String username, String password) throws InterruptedException {
-        webDriver = new ChromeDriver();
-        webDriver.get("http://localhost:2902/demo");
+        webDriver.get("https://demo.guru99.com/v4/");
         webDriver.manage().window().maximize();
         Thread.sleep(4000);
-
+        webDriver.findElement(By.xpath("//input[@name='uid']")).sendKeys(username);
+        webDriver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
     }
 
     @When("User hit login button")
     public void userHitLoginButton() {
+        webDriver.findElement(By.xpath("//input[@name='btnLogin']")).click();
     }
 
-    @Then("User is navigated to home page")
-    public void userIsNavigatedToHomePage() {
+    @Then("^User is navigated to home page and equals \"(.*)\"$")
+    public void userIsNavigatedToHomePage(String username) throws InterruptedException {
+        Thread.sleep(4000);
+        WebElement welcomeLine = webDriver.findElement(By.cssSelector("tr[class='heading3'] td"));
+        assertEquals(welcomeLine.getText(), "Manger Id : " + username);
         webDriver.close();
     }
 }
--- src/test/resources/features/Demo.feature
+++ src/test/resources/features/Demo.feature
@@ -3,14 +3,10 @@ Feature: Demo Login function
   Scenario Outline: Login with several username and password expected login successfully
 	Given User enters "<username>" and "<password>"
 	When User hit login button
-	Then User is navigated to home page
+	Then User is navigated to home page and equals "<username>"
 	Examples:
-	  | username | password |
-	  | nguyenne | 123456   |
-	  | nguyenne | 123456   |
-	  | nguyenne | 123456   |
-	  | nguyenne | 123456   |
-	  | nguyenne | 123456   |
+	  | username   | password |
+	  | mngr510570 | pedetEj  |
 
   Scenario: Log out normal
 	Given User enter log out button
